8138f4082cf83987043c0327f97eae0fefa9f6e2
Noor Ul Huda <nh958829@gmail.com>
2024-10-14 14:47:03 +0500

    first try

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Health_Hub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
