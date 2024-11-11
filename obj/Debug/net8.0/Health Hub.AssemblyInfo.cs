fd62856a25d3370e0493f773e8ace7956b5e33bf
HP <fr141277@gmail.com>
2024-11-12 02:18:51 +0500

    Pushing After Correcting the Notification Error in Doctors and make the code adaptable to the null notifications in the layout login patient.

--- obj/Debug/net8.0/Health Hub.AssemblyInfo.cs
+++ obj/Debug/net8.0/Health Hub.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Health Hub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab948b6d1ea3f4f1e6c8e51861684e1da7ffcd97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a303c35830ecfbf9fe547ee94bcbc5ccafe5df75")]
 [assembly: System.Reflection.AssemblyProductAttribute("Health Hub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Health Hub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
