d8d12a9644295cea89865f7de0d1403a86be8311
Leena Zaheer <leenazaheer734@gmail.com>
2024-11-16 11:17:40 +0500

    REsolving conflicts

--- obj/Debug/net8.0/Health Hub.AssemblyInfo.cs
+++ obj/Debug/net8.0/Health Hub.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Health Hub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef7356b0a7608acaf3d735f967ecb270f97aa6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd51ec652eb1caedd9f4216e98a9a5be603565c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Health Hub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Health Hub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
