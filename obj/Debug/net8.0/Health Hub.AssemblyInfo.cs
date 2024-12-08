39a7f176f20d2af94c7ba26d3b3ea5e59ccc43b9
HP <fr141277@gmail.com>
2024-12-08 21:52:28 +0500

    Fix Styles in Appointment page.

--- obj/Debug/net8.0/Health Hub.AssemblyInfo.cs
+++ obj/Debug/net8.0/Health Hub.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Health Hub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32acd60533dfc437378181dc9f4d4a25f0a6b5cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b746f37f7abb6b00c389a145e95ade8da608c7c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Health Hub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Health Hub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
